e9bd9497de9830cefb50426da2ab44481c2a5be6
Dhairya <dhairya00798@gmail.com>
2021-01-27 20:57:38 +0530

    01 project done

--- 01 - Expanding Cards/script.js
+++ 01 - Expanding Cards/script.js
@@ -1,1 +1,14 @@
+const panels = document.querySelectorAll(".panel");
 
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActive();
+    panel.classList.add("active");
+  });
+});
+
+removeActive = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
